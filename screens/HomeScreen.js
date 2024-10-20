661050632e672b25acc89b66d1deae1357a496b6
sharifdzon <sarifdzontahirov@gmail.com>
2024-10-20 19:34:50 +0500

    Изменения

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useLayoutEffect} from 'react';
-import { View, Text, StyleSheet, FlatList, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ScrollView, Image, TouchableOpacity } from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 import {SafeAreaView} from "react-native-safe-area-context";
 
@@ -9,13 +9,44 @@ const HomeScreen = () => {
 
     useLayoutEffect(() => {
         navigation.setOptions({ headerShown: false });
-    }, {
-
     })
 
     return (
-        <SafeAreaView>
-            <Text>Hello world</Text>
+        <SafeAreaView className="bg-white flex-1 relative">
+            {/*Первая часть*/}
+            <View className="flex-row px-6 mt-6 items-center space-x-2">
+                <View className="w-16 h-16 bg-[#f5d9bf] rounded-full items-center justify-center">
+                    <Text className="text-[#fff] text-3xl font-semibold">Go</Text>
+                </View>
+                <Text className="text-3xl font-semibold">Путешествовать</Text>
+            </View>
+            {/*Вторая часть*/}
+            <View className="px-6 mt-12 space-y-3">
+                <Text className="text-black text-[24px]">Наслаждайтесь поездкой вместе с</Text>
+                <Text className="text-[30px] font-bold text-[#f5d9bf]">Хорошими моментами</Text>
+
+                <Text className="text-base">Наше travel-приложение — это удобный и функциональный инструмент
+                    для планирования и организации поездок.</Text>
+            </View>
+            {/*Раздел с кругами*/}
+            <View className="w-[300px] h-[300px] bg-[#9ed496] rounded-full absolute bottom-20 -right-44"></View>
+            <View className="w-[300px] h-[300px] bg-amber-500 rounded-full absolute -bottom-28 -left-40"></View>
+            {/*Раздел со мной*/}
+            <View className="flex-1 relative items-center justify-center">
+                <Image
+                    source={require('../assets/me.png')}
+                    className="w-full h-full object-cover mt-32"
+                />
+                {/*Последний раздел*/}
+                <View className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[white] rounded-full justify-center items-center">
+                    <TouchableOpacity>
+                        <View className="w-20 h-20 items-center justify-center rounded-full bg-white">
+                            <Text className="font-semibold text-[30px]">GO</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
         </SafeAreaView>
     )
 }
